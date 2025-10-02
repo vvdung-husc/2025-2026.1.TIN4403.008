6e358c90718aa51439674249cf99c18072877820
hovandien2004 <hovandien1812@gmail.com>
2025-10-02 23:50:22 +0700

    hvdien

--- hovandien_22t1020067/project_02/app/src/main/java/com/example/baitap/MainActivity.java
+++ hovandien_22t1020067/project_02/app/src/main/java/com/example/baitap/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.baitap;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etUsername, etPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+            String user = etUsername.getText().toString();
+            String pass = etPassword.getText().toString();
+
+            if (user.equals("hovandien") && pass.equals("18122004")) {
+                // Đăng nhập thành công → chuyển sang HomeActivity
+                Intent intent = new Intent(MainActivity.this, HomeActivity2.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(MainActivity.this, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show();
+            }
         });
+
     }
 }
