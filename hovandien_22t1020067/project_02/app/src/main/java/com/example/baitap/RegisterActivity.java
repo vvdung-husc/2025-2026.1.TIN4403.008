60033c40d63f6e96ce9eb7130b320df1c8438eb2
hovandien2004 <hovandien1812@gmail.com>
2025-10-03 15:58:10 +0700

    dien

--- hovandien_22t1020067/project_02/app/src/main/java/com/example/baitap/RegisterActivity.java
+++ hovandien_22t1020067/project_02/app/src/main/java/com/example/baitap/RegisterActivity.java
@@ -1,24 +1,42 @@
 package com.example.baitap;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText etRegUsername, etRegPassword, etRegConfirmPassword, etRegEmail;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etRegUsername = findViewById(R.id.etRegUsername);
+        etRegPassword = findViewById(R.id.etRegPassword);
+        etRegConfirmPassword = findViewById(R.id.etRegConfirmPassword);
+        etRegEmail = findViewById(R.id.etRegEmail);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(v -> {
+            String username = etRegUsername.getText().toString();
+            String password = etRegPassword.getText().toString();
+            String confirmPass = etRegConfirmPassword.getText().toString();
+            String email = etRegEmail.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty() || confirmPass.isEmpty() || email.isEmpty()) {
+                Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+            } else if (!password.equals(confirmPass)) {
+                Toast.makeText(this, "Mật khẩu không khớp", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Đăng ký thành công cho tài khoản: " + username, Toast.LENGTH_LONG).show();
+                finish(); // Quay lại trang đăng nhập
+            }
         });
     }
 }
