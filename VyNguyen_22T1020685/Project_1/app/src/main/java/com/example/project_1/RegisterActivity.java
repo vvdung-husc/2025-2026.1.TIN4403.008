8ee51cc8f82466083b949de1bdda574799306b8a
NguyenVyNguyen <nguyenkuku990@gmail.com>
2025-10-03 16:01:04 +0700

    up

--- VyNguyen_22T1020685/Project_1/app/src/main/java/com/example/project_1/RegisterActivity.java
+++ VyNguyen_22T1020685/Project_1/app/src/main/java/com/example/project_1/RegisterActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class activity_register extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
