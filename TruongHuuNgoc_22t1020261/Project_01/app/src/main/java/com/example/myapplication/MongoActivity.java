176a4e88b101738ec1849b4479c1e765be5e5150
thngoc0000 <ngoctr2k4@gamil.com>
2025-11-21 15:47:59 +0700

    Add nodeJS

--- TruongHuuNgoc_22t1020261/Project_01/app/src/main/java/com/example/myapplication/MongoActivity.java
+++ TruongHuuNgoc_22t1020261/Project_01/app/src/main/java/com/example/myapplication/MongoActivity.java
@@ -15,6 +15,10 @@ public class MongoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mongo);
+
+        var connectionString = MongoClientURI("mongodb://172.16.96.173:27017");
+        var mongoClient = MongoClient(connectionString);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
