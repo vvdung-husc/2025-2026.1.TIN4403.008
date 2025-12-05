c13411e4dcaf432978401886f4ce3fd0ca79433c
vvdung <vvdung@hueuni.edu.vn>
2025-12-05 13:57:14 +0700

    vvdung

--- vvdung/project_01/app/src/main/java/com/example/project_01/UserActivity.java
+++ vvdung/project_01/app/src/main/java/com/example/project_01/UserActivity.java
@@ -2,15 +2,23 @@ package com.example.project_01;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserActivity extends AppCompatActivity {
     TextView m_txtWelcome;
@@ -25,8 +33,8 @@ public class UserActivity extends AppCompatActivity {
         m_txtWelcome = (TextView)findViewById(R.id.txtWelcome);
         m_btnLogout = (Button) findViewById(R.id.btnLogout);
 
-        String s = "Chào mừng tài khoản : " + MainActivity._userNameLogined;
-        m_txtWelcome.setText(s);
+        //String s = "Chào mừng tài khoản : " + MainActivity._userNameLogined;
+        //m_txtWelcome.setText(s);
         m_btnLogout.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -37,6 +45,41 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 
+        // chạy trên thread khác với UIThread để tránh bị treo ứng dụng
+        new Thread(() -> {
+            Map<String, String> headers = new HashMap<>();
+            headers.put("token", MainActivity._token);
+
+            ApiClient.ApiResult r = ApiClient.httpPost("https://dev.husc.edu.vn/tin4403/api/userinfo",null, headers);
+
+            runOnUiThread(() -> {
+                if (r.success) {
+                    Log.d("API", "OK: " + r.body);
+                    try {
+                        Log.d("API", "OK: " + r.body);
+
+                        JSONObject obj = new JSONObject(r.body);
+
+                        int ret = obj.getInt("r");
+                        JSONObject m = obj.getJSONObject("m");
+
+                        //String username = m.getString("username");
+                        //String password = m.getString("password");
+                        String fullname = m.getString("fullname");
+
+                        String s = "Chào mừng tài khoản : " + fullname;
+                        m_txtWelcome.setText(s);
+
+                    } catch (JSONException e) {
+                        Toast.makeText(this, "Lỗi ParseJSON ", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "ERR: " + r.body, Toast.LENGTH_SHORT).show();
+                    //Log.d("API", "ERR: " + r.body);
+                }
+            });
+
+        }).start();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
