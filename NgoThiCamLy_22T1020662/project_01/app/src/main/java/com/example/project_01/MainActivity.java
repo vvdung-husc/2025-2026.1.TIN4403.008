19df334da0b86f7fc303e5aa4754842c3d704f0a
Ngo Thi Cam Ly <ntcly942018@gmail.com>
2025-12-06 16:50:26 +0700

    project_01

--- NgoThiCamLy_22T1020662/project_01/app/src/main/java/com/example/project_01/MainActivity.java
+++ NgoThiCamLy_22T1020662/project_01/app/src/main/java/com/example/project_01/MainActivity.java
@@ -1,46 +1,163 @@
 package com.example.project_01;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class MainActivity extends AppCompatActivity {
+    static String _token; //token nhận được khi login thành công
+    EditText m_edtUser, m_edtPass; //EditText
+    Button m_btnLogin, m_btnRegister; //Button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        EditText edtNhapTaiKhoan = findViewById(R.id.id_TaiKhoan);
-        EditText edtNhapMatKhau = findViewById(R.id.id_MatKhau);
-        Button btnDangNhap = findViewById(R.id.id_DangNhap);
-        Button btnDangKy = findViewById(R.id.id_DangKi);
+        //Khởi tạo các biến điều khiển tương ứng trong layout
+        m_edtUser = (EditText) findViewById(R.id.edtUsername);
+        m_edtPass = (EditText) findViewById(R.id.edtPassword);
+        m_btnLogin = (Button) findViewById(R.id.btnLogin);
+        m_btnRegister = (Button) findViewById(R.id.btnRegister);
 
-        // ✅ Xử lý nút "Đăng nhập"
-        btnDangNhap.setOnClickListener(v -> {
-            String taiKhoan = edtNhapTaiKhoan.getText().toString().trim();
-            String matKhau = edtNhapMatKhau.getText().toString().trim();
+        //Cài đặt Click cho Button Login
+        m_btnLogin.setOnClickListener(new CButtonLogin());
 
-            if (taiKhoan.isEmpty() || matKhau.isEmpty()) {
-                Toast.makeText(MainActivity.this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
-            } else if (taiKhoan.equals("admin") && matKhau.equals("123")) {
-                Toast.makeText(MainActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+        //Cài đặt Click cho Button Register
+        m_btnRegister.setOnClickListener(new CButtonRegister());
 
-                // 👉 Chuyển sang màn hình UserActivity
-                Intent intent = new Intent(MainActivity.this, UserActivity.class);
-                startActivity(intent);
-            } else {
-                Toast.makeText(MainActivity.this, "Sai tài khoản hoặc mật khẩu!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // ✅ Xử lý nút "Đăng ký"
-        btnDangKy.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-            startActivity(intent);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
+
+    public class CButtonLogin implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {//Hàm sử lý click button login
+            String user = m_edtUser.getText().toString();// lấy thông tin nhâp tài khoản đã nhập
+            String pass = m_edtPass.getText().toString();// lấy thông tin mật khẩu đã nhập
+            Log.d("K46", "CLICK BUTTON LOGIN ACCOUNT " + user + "/" + pass);
+            if (user.length() < 3 || pass.length() < 6) {
+                Toast.makeText(getApplicationContext(), "Tài khoản hoặc mật khẩu không hợp lệ!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            //Gọi hàm  Login
+            try {
+                //apiLogin(user,pass);
+                okhttpLogin(user,pass);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            //String msg = "Đã nhập thông tin tài khoản [" + user + "/" + pass + "]";
+            //Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class CButtonRegister implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {//Hàm sử lý  click button register
+            //Toast.makeText(getApplicationContext(),"::onClick...",Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(i);
+        }
+    }
+
+    //Hàm Login
+    void okhttpLogin(String user, String pass) throws IOException {
+        //boolean bOk = (user.equals("vvdung") && pass.equals("123456"));
+        String json = "{\"username\":\"" + user + "\",\"password\":\"" + pass +"\"}";
+        Toast.makeText(getApplicationContext(),json,Toast.LENGTH_SHORT).show();
+        Log.d("K46",json);
+
+        // chạy trên thread khác với UIThread để tránh bị treo ứng dụng
+        new Thread(() -> {
+            ApiClient.ApiResult r = ApiClient.httpPost("https://dev.husc.edu.vn/tin4403/api/login", json,null);
+
+            runOnUiThread(() -> {
+                if (r.success) {
+                    Toast.makeText(this, "OK: " + r.body, Toast.LENGTH_SHORT).show();
+
+                    try {
+                        JSONObject obj = new JSONObject(r.body);
+
+                        int ret = obj.getInt("r");
+                        String msg = obj.getString("m");   // m là Base64
+
+                        _token = msg;
+
+                        Intent i = new Intent(getApplicationContext(), UserActivity.class);
+                        startActivity(i);
+                    } catch (JSONException e) {
+                        Toast.makeText(this, "Lỗi ParseJSON ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                    Toast.makeText(this, "ERR: " + r.body, Toast.LENGTH_SHORT).show();
+            });
+        }).start();
+
+    }
+
+    /*void apiLogin(String user, String pass) throws IOException {
+        //boolean bOk = (user.equals("vvdung") && pass.equals("123456"));
+        String json = "{\"username\":\"" + user + "\",\"password\":\"" + pass +"\"}";
+        Toast.makeText(getApplicationContext(),json,Toast.LENGTH_SHORT).show();
+        Log.d("K46",json);
+
+        RequestBody body = RequestBody.create(json,JSON);
+        Request request = new Request.Builder()
+                .url("https://dev.husc.edu.vn/tin4403/api/login") //.url("http://192.168.56.1:4380/login")
+                .post(body)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String errStr = "Tài khoản hoặc mật khẩu không chính xác.\n" + e.getMessage();
+                Log.d("K46","onFailure\n" + errStr);
+                Toast.makeText(getApplicationContext(),errStr,Toast.LENGTH_SHORT).show();
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String errStr = "Tài khoản hoặc mật khẩu không chính xác.\n" + response.body().string();
+                Log.d("K46",errStr);
+                if (!response.isSuccessful()){
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),errStr,Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    return;
+                }
+
+                Intent intent = new Intent(getApplicationContext(),UserActivity.class);
+                startActivity(intent);
+
+            }
+        });//client.newCall(request).enqueue(new Callback() {
+    }*/
+
 }
