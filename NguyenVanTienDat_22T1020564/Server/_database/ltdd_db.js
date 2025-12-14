0fb58eccf48c6d59e579319076cab719612dfd41
tiendat0206 <22T1020564@husc.edu.vn>
2025-12-14 15:07:40 +0700

    update

--- NguyenVanTienDat_22T1020564/Server/_database/ltdd_db.js
+++ NguyenVanTienDat_22T1020564/Server/_database/ltdd_db.js
@@ -10,11 +10,9 @@ function CDBLTDD() {
     this.db_ = null;
 }
 
-CDBLTDD.prototype.Init = async function (callback) {
+CDBLTDD.prototype.Init = async function () {
     console.log('Connecting LTDD Database...');
-
-    try {
-        
+    try {        
         await this.client_.connect();
         console.log("Connected to MongoDB!");
         //console.log(this.client_);
@@ -23,21 +21,34 @@ CDBLTDD.prototype.Init = async function (callback) {
         //console.log(this.db_);
         console.log('...MONGO Actived : [' + this.db_.databaseName + ']');
 
-        const user = await this.getByUsername('bachtt_k46','020543');
-        console.log(user);
+        // const user = await this.getByUsername('bachtt_k46','020543');
+        // console.log(user);
 
-        callback();
+        return true;
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
+        return false;
     }
 }
 
-CDBLTDD.prototype.getUsers = async function(){
-  const users = await this.db_.collection("users").find().toArray();
+CDBLTDD.prototype.getUser = async function(user){
+  const u = await this.db_.collection("users").findOne({username:user});
+  return u;   // trả về user hoặc null
+}
+
+CDBLTDD.prototype.getUsers = async function(){//không trả về _id
+  const users = await this.db_.collection("users").find({}, { projection: { _id: 0 } }).toArray();
   return users;   // trả về mảng users
 }
 
-CDBLTDD.prototype.getByUsername = async function(user, pass){
+CDBLTDD.prototype.Authentication = async function(user, pass){
   const u = await this.db_.collection("users").findOne({ username: user, password: pass});
   return u;   // trả về user hoặc null
 }
+
+CDBLTDD.prototype.modifyUser = async function (user, modify){
+    //console.log(modify);
+    const oDoc = await this.db_.collection("users").updateOne({username:user},{$set:modify});
+    //console.log(oDoc);
+    return oDoc;
+} 
