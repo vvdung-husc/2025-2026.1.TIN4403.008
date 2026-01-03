4fcd3c7c286890ac8489443909c7d72a6187f799
Hồ Xuân Bắc <22t1020541@husc.edu.vn>
2026-01-03 17:10:38 +0700

    up

--- HoXuanBac_22T1020541/project1/app/src/androidTest/java/com/example/project1/ExampleInstrumentedTest.java
+++ HoXuanBac_22T1020541/project1/app/src/androidTest/java/com/example/project1/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.project1", appContext.getPackageName());
+        assertEquals("com.example.projects1", appContext.getPackageName());
     }
 }
