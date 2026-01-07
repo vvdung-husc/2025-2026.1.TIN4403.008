c83f5fe3877637cb68828504ad5056e739a86d4d
Nguyen Thanh Dat <22t1020569@husc.edu.vn>
2026-01-07 18:07:22 +0700

    up

--- NguyenThanhDat_22T1020569/Project/app/src/main/java/com/example/project1/ApiClient.java
+++ NguyenThanhDat_22T1020569/Project/app/src/main/java/com/example/project1/ApiClient.java
@@ -21,10 +21,10 @@ public class ApiClient {
 
     // ĐÂY LÀ ĐỊNH NGHĨA URL CÁC API CHO APP DÙNG MẠNG NỘI BỘ - THAY ĐỊA CHỈ IP VÀ PORT ĐÚNG VỚI DỊCH VỤ ĐANG CHẠY
 
-    public static final String URL_LOGIN = "http://26.233.221.196/login";
-    public static final String URL_USER_INFO = "http://26.233.221.196/userinfo";
-    public static final String URL_USER_UPDATE = "http://26.233.221.196/userupdate";
-    public static final String URL_USER_REGISTER = "http://26.233.221.196/register";
+    public static final String URL_LOGIN = "http://10.118.3.49/login";
+    public static final String URL_USER_INFO = "http://10.118.3.49/userinfo";
+    public static final String URL_USER_UPDATE = "http://10.118.3.49/userupdate";
+    public static final String URL_USER_REGISTER = "http://10.118.3.49/register";
 
     private static final OkHttpClient client = new OkHttpClient();
 
